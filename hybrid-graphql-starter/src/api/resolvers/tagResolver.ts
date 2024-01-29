ced884a9359caf3a138f238a30d77846d7c39c92
antti-riiko <riikonen.j.antti@gmail.com>
2024-01-29 23:45:28 +0200

    toka

---
+++ hybrid-graphql-starter/src/api/resolvers/tagResolver.ts
@@ -0,0 +1,32 @@
+import {Tag} from '@sharedTypes/DBTypes';
+import {
+  deleteTag,
+  fetchAllTags,
+  fetchTagsByMediaId,
+  postTag,
+} from '../models/tagModel';
+
+export default {
+  MediaItem: {
+    tags: async (parent: {media_id: string}) => {
+      return await fetchTagsByMediaId(Number(parent.media_id));
+    },
+  },
+  Query: {
+    tags: async () => {
+      return await fetchAllTags();
+    },
+  },
+  Mutation: {
+    createTag: async (
+      _parent: undefined,
+      args: {input: Omit<Tag, 'tag_id'>},
+    ) => {
+      console.log(args);
+      return await postTag(args.input);
+    },
+    deleteTag: async (_parent: undefined, args: {input: string}) => {
+      return await deleteTag(Number(args.input));
+    },
+  },
+};
